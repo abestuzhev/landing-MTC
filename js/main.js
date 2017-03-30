000b77ac1bbc28be15d3d1c943ebf046fff56d20
Aleksey <aleksey.bestuzhev@gmail.com>
2017-03-30 22:10:05 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-  $(".top-header__nav-link").click(function() {
-  	$(".top-header__nav-link").removeClass("top-header__nav-link_active");
-  	$(this).addClass("top-header__nav-link_active");
-  });
+  // $(".top-header__nav-link").click(function() {
+  // 	$(".top-header__nav-link").removeClass("top-header__nav-link_active");
+  // 	$(this).addClass("top-header__nav-link_active");
+  // });
 
   $(".top-header__mobile-menu").click(function() {
   	$(this).toggleClass("change");
@@ -11,5 +11,3 @@ $(document).ready(function() {
   	$(".overley-page").fadeToggle();
   });
 });
-
-
